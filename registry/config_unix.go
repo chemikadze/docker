8b7e093f5c7f6449ecd3183b1b2b93d51b6cf261
Nikolay Sokolov <chemikadze@gmail.com>
2016-03-31 11:52:34 -0700

    DOCKER_REGISTRY_CERTS_PATH

--- registry/config_unix.go
+++ registry/config_unix.go
@@ -2,6 +2,10 @@
 
 package registry
 
+import (
+	"os"
+)
+
 const (
 	// DefaultV1Registry is the URI of the default v1 registry
 	DefaultV1Registry = "https://index.docker.io"
@@ -12,8 +16,17 @@ const (
 
 var (
 	// CertsDir is the directory where certificates are stored
-	CertsDir = "/etc/docker/certs.d"
+	// "/etc/docker/certs.d"
+	CertsDir = certsDir()
 )
+
+func certsDir() string {
+	path := os.Getenv("DOCKER_REGISTRY_CERTS_PATH")
+	if len(path) == 0 {
+		path = "/etc/docker/certs.d"
+	}
+	return path
+}
 
 // cleanPath is used to ensure that a directory name is valid on the target
 // platform. It will be passed in something *similar* to a URL such as
